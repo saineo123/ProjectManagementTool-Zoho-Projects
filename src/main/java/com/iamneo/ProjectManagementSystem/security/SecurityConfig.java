1b28ac7b18194574c078315da9d3d6468785e763
Rishab <rishab.savanth@iamneo.ai>
2024-04-13 14:14:13 +0530

    srcurity

---
+++ src/main/java/com/iamneo/ProjectManagementSystem/security/SecurityConfig.java
@@ -0,0 +1,51 @@
+
+package com.iamneo.ProjectManagementSystem.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+
+@Configuration
+@EnableMethodSecurity(securedEnabled = true)
+public class SecurityConfig {
+    
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder){
+        UserDetails admin = User.withUsername("admin")
+                            .password(encoder.encode("password"))
+                            .roles("ADMIN","USER")
+                            .build();
+        UserDetails user = User.withUsername("user")
+                            .password(encoder.encode("12345"))
+                            .roles("USER")
+                            .build();
+
+            return new InMemoryUserDetailsManager(admin,user);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http
+            .csrf(AbstractHttpConfigurer::disable)
+            .formLogin(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(authorizationManagerRequestMatchRegistry -> authorizationManagerRequestMatchRegistry .anyRequest().authenticated())
+            .httpBasic(withDefaults())
+            .build();
+    }
+}
