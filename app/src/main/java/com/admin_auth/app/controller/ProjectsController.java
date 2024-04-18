5d0d1f9b16e6ff54843567dfe17ac605da201881
saineo123 <163998958+saineo123@users.noreply.github.com>
2024-04-18 12:02:57 +0530

    Update ProjectsController.java
    Created a new feature of creating a project

--- app/src/main/java/com/admin_auth/app/controller/ProjectsController.java
+++ app/src/main/java/com/admin_auth/app/controller/ProjectsController.java
@@ -1,16 +1,16 @@
 package com.admin_auth.app.controller;
 
 import java.time.LocalDate;
-
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,11 +18,11 @@ import java.time.temporal.ChronoUnit;
 import com.admin_auth.app.model.Projects;
 import com.admin_auth.app.services.ProjectService;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("/auth/projects")
 public class ProjectsController {
+    //private static final int Set = 0;
     @Autowired
     private ProjectService projectService;
 
@@ -49,8 +49,8 @@ public class ProjectsController {
         }
     
         // Convert Date to LocalDate
-        LocalDate localStartDate = project.getStartDate().toLocalDate();
-        LocalDate localEndDate = project.getEndDate().toLocalDate();
+        LocalDate localStartDate = project.getStartDate();
+        LocalDate localEndDate = project.getEndDate();
     
         // Calculate the duration between the two dates
         long totalDays = ChronoUnit.DAYS.between(localStartDate, localEndDate);
@@ -61,5 +61,16 @@ public class ProjectsController {
         return ResponseEntity.ok(progress);
     }
 
+    
+    @PostMapping("/create")
+    @Secured("ROLE_ADMIN")
+    public ResponseEntity<Object> createProject(@RequestBody Projects projects){
+        //String title = projects.getTitle();
+        Projects createProject = projectService.saveProjects(projects);
+        //Set<Projects> set = new HashSet<>();
+        //set.add(createProject);
+        return new ResponseEntity<Object>(createProject,HttpStatus.CREATED);
+
+    }
 
 }
