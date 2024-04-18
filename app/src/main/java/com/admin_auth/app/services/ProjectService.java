8768cca8c822a593fe0134a01b9fc4f866e8f4aa
saineo123 <163998958+saineo123@users.noreply.github.com>
2024-04-18 12:03:41 +0530

    Update ProjectService.java
    added a new method of saving the project

--- app/src/main/java/com/admin_auth/app/services/ProjectService.java
+++ app/src/main/java/com/admin_auth/app/services/ProjectService.java
@@ -1,7 +1,9 @@
 package com.admin_auth.app.services;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
@@ -14,6 +16,9 @@ import com.admin_auth.app.repository.ProjectRepo;
 public class ProjectService {
     @Autowired
     private ProjectRepo project_repo;
+
+    /// ---------------------------------------------------
+    Set<Projects> set = new HashSet<>();
 
     public List<Projects> getAllProjects(){
         return project_repo.findAll();
@@ -34,5 +39,23 @@ public class ProjectService {
     public Projects saveProjects(Projects projects){
         return project_repo.save(projects);
     }
+
+    // @Secured("ROLE_ADMIN")
+    // public Projects saveProjects(Projects projects) {
+    //     // Check if a project with the same title, start date, end date, and description exists
+    //     Projects existingProject = (Projects) project_repo.findAll();
+
+    //     // If a duplicate project is found, you can handle it accordingly
+    //     if (existingProject != null) {
+    //         // For example, you can throw an exception, log a message, or return null
+    //         // Here, we'll throw an exception
+    //         //throw new DuplicateProjectException("Project with the same title, start date, end date, and description already exists");
+    //         System.out.println("Project already exists");
+    //     }
+
+    //     // If no duplicate project is found, save the new project
+    //     return project_repo.save(projects);
+    // }
+    
     
 }
